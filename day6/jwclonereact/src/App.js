500591d8a7eb238c1ed91c895b25744e721e5ec3
isfansy <isfansyahreza@gmail.com>
2019-11-27 14:02:40 +0700

    day6

---
+++ day6/jwclonereact/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class  extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+        <section className="section1">
+          <header/>
+          <Jumbotron1/>
+
+        </section>
+      </div>
+     );
+  }
+}
+ 
+
+export default App;
